a09ed0f7722012f474035ea60342dcc646034dae
DomVes <juan.cc237@gmail.com>
2022-04-06 05:29:05 -0500

    CategoryCreate

--- GlampingITM/GlampingITM/Program.cs
+++ GlampingITM/GlampingITM/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddDbContext<DataContext>(o =>
     o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 if (!app.Environment.IsDevelopment())
